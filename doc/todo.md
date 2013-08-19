6b4f75916e74e9145ecba9773628bcf4e2d6c522
tumregels <tumregels@gmail.com>
2013-08-19 10:00:13 +0200

    adds todo.md and fuelpinburn testcase

---
+++ doc/todo.md
@@ -0,0 +1,41 @@
+URGENT
+=======
+
+1. Add burnup module
+
+2. add error checking for forgotten '<>' in makeSerpInp.m
+
+3. finalize XSEC for SFR and compare XSEC with the old one.
+
+HIGH PRIORITY
+=============
+
+1. adjust saveResults function to save only results and staged structure.
+
+2. '-delcr_base' check for SFR
+
+3. add inpPar(i) structure to the results(i) structure 
+    and modify dataOptimizer.
+
+4. create symlink for the Serpent Libraries and change them in the code(fail)
+
+MEDIUM PRIORITY
+===============
+
+1. README.md
+
+2. modify KFISS in dataOptimizer.m (add set and get methods)
+
+3. write mdivide function error
+
+
+LOW PRIORITY
+============
+
+1. add RSTD to the plus minus rdivide and times of xsec
+
+2. comment the function getSimComplStatus
+
+3. add test_data folders and their content i.e modify .gitignore
+
+4. write bash script generator
--- examples/fuelpinburn/data_for_fuelpinburn.m
+++ examples/fuelpinburn/data_for_fuelpinburn.m
@@ -1,50 +1,53 @@
-clc;close all;
+clc; clear all; close all;
 
-TITLE = '"FPburn    Sodium cooled pin cell with (U,Pu)O2 fuel"';
-
-HEXR = [0.60, 0.62, 0.63];
-FH = [ 0, 10, 20];
-FDC = [1.0, 1.1, 1.2];
-VG = [0.01, 0.015, 0.02];
+[path, name, ext] = fileparts(...
+    '/home/asikth/Documents/S2P/stop/examples/fuelpinburn/fuelpinburn.spi');
 
 genData.serpCallCommand = 'sss1118';
-genData.serpParInpDir = '/home/asikth/Documents/S2P/stop/examples/fuelpin';
-genData.serpParInpName = 'fuelpinburn.spi';
+genData.serpParInpDir = path;
+genData.serpParInpName = [name ext];
 genData.stopDir = '/home/asikth/Documents/S2P/stop/';
 genData.saveDir = genData.serpParInpDir;
 
-genData.saveResPar = {...
-    'CHI';'CHID';'BETA_EFF';'P1_TRANSPXS';'RABSXS';'NSF';...
-    'FISSXS';'FISSE';...
-    'GPRODXS';'RECIPVEL';...
-    'ADFS';'ADFC';...
-    'PRECURSOR_GROUPS';'BETA_EFF';'BETA_ZERO';'DECAY_CONSTANT'...
-    };
-genData.saveDetPar = {''};
+genData.saveResPar = {'ANA_KEFF';'IMP_KEFF'};
 
-
-genData.eneGroups = int8(2);
 genData.isTest  = false;
-genData.isLog = false;
-genData.isExtract = false;
-genData.isBurnup = false;
 genData.isContinue = false;
 
-inpPar.HEXR = 0;
-inpPar.FH = 0;
-inpPar.FDC = 0;
-inpPar.TITLE = TITLE;
+% list of parameters to be used during simulation
+
+TITLE = '"FP    Sodium cooled pin cell with (U,Pu)O2 fuel"';
+
+HEXR = [0.60, 0.62, 0.63];
+FH =   [   0,   10,   20];
+FDC =  [ 1.0,  1.1,  1.2];
+VG =   [0.01,0.015, 0.02];
+
+% prepare data structure for 'stop' wrapper
 
 for n = 1:length(HEXR)
+    
     calc(n).genData = genData;
-    calc(n).inpPar = inpPar;
+    calc(n).genData.calcNum = n;
+    
     calc(n).inpPar.HEXR = HEXR(n);
     calc(n).inpPar.FH = FH(n);
     calc(n).inpPar.FDC = FDC(n);
     calc(n).inpPar.VG = VG(n);
+    calc(n).inpPar.TITLE = TITLE;
+    
 end
 
+% add all folders of the stop wrapper to matlab search path
+
 addpath(genpath(genData.stopDir))
-stom(calc(1));
+
+% perform simulation
+
+for n = 1:length(HEXR)
+    
+    [results(n), ~] = stom(calc(n));
+    
+end
 
 
