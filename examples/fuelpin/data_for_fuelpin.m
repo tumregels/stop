5c3dcee0d1c210495272f746cb6ab7ae2e526fba
tumregels <tumregels@gmail.com>
2013-08-29 13:16:55 +0200

    adds safety coefficients for fuelpin

--- examples/fuelpin/data_for_fuelpin.m
+++ examples/fuelpin/data_for_fuelpin.m
@@ -2,14 +2,14 @@ clc; clear all; close all;
 
 calc.serpExe = 'sss1118';
 calc.stopDir = '/home/asikth/Documents/S2P/stop/';
-calc.serpParInp.dir = pwd;
 calc.serpParInp.name = 'fuelpin.spi';
+calc.serpParInp.dir = pwd;
 calc.saveDir = pwd;
 
 calc.saveResPar = {'ANA_KEFF';'IMP_KEFF'};
 
 calc.isTest  = false;
-calc.isContinue = false;
+calc.isContinue = true;
 calc.isEcho = true;
 
 
@@ -17,7 +17,6 @@ calc(1:4) = calc;
 
 calc(1).name = 'ref';
 calc(1).values = struct('TITLE','fuelpin1',...
-    'HEXR',0.60,...
     'FH',0,...
     'FDC',1,...
     'FT','09',...
@@ -25,7 +24,6 @@ calc(1).values = struct('TITLE','fuelpin1',...
 
 calc(2).name = 'fhhi';
 calc(2).values = struct('TITLE','fuelpin2',...
-    'HEXR',0.60,...
     'FH',[3.4174,3000],...% fuel hight increase at 3000K
     'FDC',[0.9670, 3000],...% fuel density coefficient at 3000K
     'FT','09',...
@@ -33,7 +31,6 @@ calc(2).values = struct('TITLE','fuelpin2',...
 
 calc(3).name = 'fthi';
 calc(3).values = struct('TITLE','fuelpin3',...
-    'HEXR',0.60,...
     'FH',0,...
     'FDC',1,...
     'FT','18',...
@@ -41,7 +38,6 @@ calc(3).values = struct('TITLE','fuelpin3',...
 
 calc(4).name = 'cdlow';
 calc(4).values = struct('TITLE','fuelpin3',...
-    'HEXR',0.60,...
     'FH',0,...
     'FDC',1,...
     'FT','09',...
@@ -61,33 +57,70 @@ for n = 1:length(calc)
     
 end
 
-% generate xsec card
+%%calculate doppler constant
 
-if calc(1).isTest == false
-    
-    s1 = results(1).res;
-    s2 = results(2).res;
-    s3 = results(3).res;
-    
-    s1 = xsec.dataOptimizer(s1);
-    s2 = xsec.dataOptimizer(s2);
-    s3 = xsec.dataOptimizer(s3);
-    
-    card = xsec();
-    card.comp_num(1);
-    card.base_macro(s1,1);
-    card.dxs_dtf(s1,1);
-    card.dxs_ddm(s1,1);
-    card.dxs_axexp(s1,1);
-    card.dxs_radexp(s1,1);
-    
-    card.comp_num(2);
-    card.base_macro(s2,2);
-    
-    s3 = (s1 + s2)./10;
-    card.comp_num(3);
-    card.base_macro(s3,3);
-    card.display();
-    card.write();
-    
-end
+keff_ref = results(1).res.IMP_KEFF(1,1);
+keff_fthi = results(3).res.IMP_KEFF(1,1);
+
+rho_ref = 10^5*(keff_ref - 1)/keff_ref;
+rho_fthi = 10^5*(keff_fthi - 1)/keff_fthi;
+
+K_dop = (rho_ref - rho_fthi)/(log(1800) - log(900))%pcm/K
+
+% standard deviation of doppler constant
+
+std_ref = results(1).res.IMP_KEFF(1,2);
+std_fthi = results(3).res.IMP_KEFF(1,2);
+
+std_rho_ref = rho_ref*sqrt(...
+    (std_ref/(keff_ref-1))^2 + (std_ref/keff_ref)^2 );
+std_rho_fthi = rho_fthi*sqrt(...
+    (std_fthi/(keff_fthi-1))^2 + (std_ref/keff_fthi)^2 );
+
+std_K_dop = ( 1/( log(1800)-log(900) ) )*sqrt(...
+    std_rho_ref^2 + std_rho_fthi^2 )
+
+%%calculate axial fuel expansion coefficient "afec"
+
+keff_fhhi = results(2).res.IMP_KEFF(1,1);
+
+rho_fhhi = 10^5*(keff_fhhi - 1)/keff_fthi;
+
+temp_ref = 900; % Kelvin
+temp_fhhi = 3000; % Kelvin
+
+afec = ...
+    (rho_ref - rho_fhhi)/(temp_ref - temp_fhhi) %pcm/K
+
+% standard deviation of expansion coefficient
+
+std_fhhi = results(2).res.IMP_KEFF(1,2);
+
+std_rho_fhhi = rho_fhhi*sqrt(...
+    (std_fhhi/(keff_fhhi-1))^2 + (std_ref/keff_fhhi)^2 );
+
+std_afec = ( -1/(temp_ref - temp_fhhi) )*sqrt(...
+    std_rho_ref^2 + std_rho_fhhi^2 )
+
+%%calculate coolant expansion coefficient
+
+keff_cdlow = results(4).res.IMP_KEFF(1,1);
+
+rho_cdlow = 10^5*(keff_cdlow - 1)/keff_cdlow;
+
+ctemp_ref = 663;%K
+ctemp_cdlow = 1400;%K
+
+cec = (rho_ref - rho_cdlow)/(ctemp_ref - ctemp_cdlow)
+
+% standard deviation of coolant expansion coefficient
+
+std_cdlow = results(4).res.IMP_KEFF(1,2);
+
+std_rho_cdlow = rho_cdlow*sqrt(...
+    (std_cdlow/(keff_cdlow-1))^2 + (std_ref/keff_cdlow)^2 );
+
+std_cec = ( -1/(ctemp_ref - ctemp_cdlow) )*sqrt(...
+    std_rho_ref^2 + std_rho_cdlow^2 )
+
+
